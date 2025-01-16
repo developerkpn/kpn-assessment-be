36af30539142bec1ca78a4ed0fcd8f30b677822c
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-16 14:50:36 +0700

    feat: category

---
+++ models/CategoryModel.ts
@@ -0,0 +1,83 @@
+import {db} from "#dep/config/connection";
+import { TRANSACTION as TRANS } from "#dep/config/transaction";
+import {
+    deleteQuery,
+    insertQuery,
+    updateQuery,
+} from "#dep/helper/queryBuilder";
+import {CategoryRequest} from "#dep/types/MasterDataTypes";
+
+
+export const createCategory = async (payload: CategoryRequest)=> {
+    const client = await db.connect();
+    try {
+        await client.query(TRANS.BEGIN);
+        const [q, v] = insertQuery("mst_category", payload, "category_code");
+        const result = await client.query(q, v);
+        await client.query(TRANS.COMMIT);
+        return result.rows[0].category_code;
+    } catch (error) {
+        console.error(error);
+        await client.query(TRANS.ROLLBACK);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+export const getCategory = async ()=> {
+    const client = await db.connect();
+    try {
+        await client.query(TRANS.BEGIN);
+        const result = await client.query(
+            `
+            SELECT * FROM mst_category
+            `
+        );
+        await client.query(TRANS.COMMIT);
+        return result.rows;
+    } catch (error) {
+        console.error(error);
+        await client.query(TRANS.ROLLBACK);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+export const updateCategory = async (payload: CategoryRequest, id: number)=> {
+    const client = await db.connect();
+    try {
+        await client.query(TRANS.BEGIN);
+        const [q, v] = updateQuery("mst_category", payload, {id}, "category_code");
+        const result = await client.query(q, v);
+        if (result.rowCount === 0) throw new Error(`ID ${id} not exist`);
+        await client.query(TRANS.COMMIT);
+        return result.rows[0].category_code;
+    } catch (error) {
+        console.error(error);
+        await client.query(TRANS.ROLLBACK);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+export const deleteCategory = async (id: number)=> {
+    const client = await db.connect();
+    try {
+        await client.query(TRANS.BEGIN);
+        const [q, v] = deleteQuery("mst_category", { id });
+        const result = await client.query(q, v);
+        console.log(result);
+        if (result.rowCount === 0) throw new Error(`ID ${id} not exist`);
+        await client.query(TRANS.COMMIT);
+        return id;
+    } catch (error) {
+        console.error(error);
+        await client.query(TRANS.ROLLBACK);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
