e12693afb789c9f365168c02345df805099fabad
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-16 15:15:12 +0700

    fix: Category menuid on its route

--- routes/Category.ts
+++ routes/Category.ts
@@ -8,7 +8,7 @@ import {
 } from "#dep/controllers/CategoryController";
 export const Category = Router();
 
-Category.post("/", checkPermission("fcreate", 8), handleCreateCategory);
-Category.get("/", checkPermission("fread", 8), handleGetCategory);
-Category.patch("/:id", checkPermission("fupdate", 8), handleUpdateCategory);
-Category.delete("/:id", checkPermission("fdelete", 8), handleDeleteCategory);
+Category.post("/", checkPermission("fcreate", 11), handleCreateCategory);
+Category.get("/", checkPermission("fread", 11), handleGetCategory);
+Category.patch("/:id", checkPermission("fupdate", 11), handleUpdateCategory);
+Category.delete("/:id", checkPermission("fdelete", 11), handleDeleteCategory);
