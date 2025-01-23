ff8a87297737a9866a7080f70c7437ef6b636e0d
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-23 16:51:32 +0700

    feat: error middleware

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config({
-  path: `./.env.${process.env.NODE_ENV}`,
-});
+dotenv.config();
+
 import os from "os";
 import https from "https";
 import path from "path";
@@ -12,11 +11,12 @@ import fs from "fs";
 import { origins as whitelist } from "#dep/config/allowedOrigins";
 import { credentials } from "./middleware/credential";
 import router from "./routes";
+import {errorMiddleware} from "#dep/middleware/errorMiddleware";
 const app = express();
-// const servOption = {
-//   cert: fs.readFileSync("./ssl/cert.pem"),
-//   key: fs.readFileSync("./ssl/key.pem"),
-// };
+const servOption = {
+  cert: fs.readFileSync("./ssl/cert.pem"),
+  key: fs.readFileSync("./ssl/key.pem"),
+};
 
 const corsOption: CorsOptions = {
   origin: function (req, callback) {
@@ -45,15 +45,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(router);
+app.use(errorMiddleware);
 app.use(express.static(path.join(__dirname, "public/build")));
 app.get("/*$", (req, res) => {
   res.sendFile(path.join(__dirname, "public/build", "index.html"));
 });
 
-app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
-  console.log(`App running on ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
+//   console.log(`App running on ${process.env.PORT}`);
+// });
 
 // const server = https.createServer(servOption, app).listen(process.env.PORT, () => {
 //   console.log(`App running on ${process.env.PORT}`);
 // });
+
+app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
+  console.log(`App running on http://localhost:${process.env.PORT}`);
+});
---
+++ middleware/errorMiddleware.ts
@@ -0,0 +1,19 @@
+import {Response, Request, NextFunction} from "express";
+import {ZodError} from "zod";
+import {ResponseError} from "../error/response-error";
+
+export const errorMiddleware = async (error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof ZodError) {
+        res.status(400).json({
+            message: error.errors.map((err) => err.message).join(', ')
+        });
+    } else if (error instanceof ResponseError) {
+        res.status(error.status).json({
+            message: error.message
+        });
+    } else {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
