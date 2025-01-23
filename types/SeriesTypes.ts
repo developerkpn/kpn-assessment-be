af934d8b57cdb4ddf83515257fe6adaa13b660ec
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-23 15:22:41 +0700

    feat: pagination, search, and filter on series module

---
+++ types/SeriesTypes.ts
@@ -0,0 +1,32 @@
+export type SeriesRequests = {
+    series_name: string;
+    series_code: string;
+    is_active: boolean;
+    category_id: string;
+    // detail: {
+    //     question_id: string;
+    // }[];
+}
+
+export type SeriesHeaderRequest = {
+    id: string;
+    series_name: string;
+    series_code: string;
+    is_active: boolean;
+    category_id: string;
+    created_by: string;
+    created_date: Date;
+}
+
+export type SeriesQuery = {
+    search?: string;
+    active?: boolean;
+    page?: number;
+    date?: string;
+}
+
+export type SeriesDetailRequest = {
+    question_id: string;
+}
+
+
