eb491e1272022e05eb3082e5dbf35ad858794994
Ilham Hakim <ilhmhakim@gmail.com>
2025-01-24 10:17:33 +0700

    update: function menu  validation

--- controllers/FunctionMenuController.ts
+++ controllers/FunctionMenuController.ts
@@ -7,6 +7,8 @@ import {
 import { FunctionMenuRequest } from "#dep/types/MasterDataTypes";
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
+import {Validation} from "#dep/validation/Validation";
+import {FunctionMenuValidation} from "#dep/validation/FunctionMenuValidation";
 
 export const handleGetFunctionMenu = async (_req: Request, res: Response) => {
   try {
@@ -34,7 +36,8 @@ export const handleCreateFunctionMenu = async (req: Request, res: Response) => {
   };
 
   try {
-    let result = await createFunctionMenu(payload);
+    const validatedRequest = Validation.validate(FunctionMenuValidation.CREATE, payload);
+    let result = await createFunctionMenu(validatedRequest);
     res.status(200).send({
       message: `Success create function menu`,
       fm_code: result,
@@ -47,12 +50,11 @@ export const handleCreateFunctionMenu = async (req: Request, res: Response) => {
 };
 
 export const handleDeleteFunctionMenu = async (req: Request, res: Response) => {
-  const id = req.params.id;
   try {
-    let result = await deleteFunctionMenu(id);
+    const validatedId = Validation.validate(FunctionMenuValidation.ID, req.params.id);
+    await deleteFunctionMenu(validatedId);
     res.status(200).send({
       message: `Success delete function menu`,
-      id: id,
     });
   } catch (error: any) {
     res.status(500).send({
@@ -62,10 +64,10 @@ export const handleDeleteFunctionMenu = async (req: Request, res: Response) => {
 };
 
 export const handleUpdateFunctionMenu = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const payload = req.body;
   try {
-    let result = await updateFunctionMenu(payload, id);
+    const validatedRequest = Validation.validate(FunctionMenuValidation.UPDATE, req.body);
+    const validatedId = Validation.validate(FunctionMenuValidation.ID, req.params.id);
+    let result = await updateFunctionMenu(validatedRequest, validatedId);
     res.status(200).send({
       message: `Success update function menu`,
       fm_code: result,
---
+++ validation/FunctionMenuValidation.ts
@@ -0,0 +1,20 @@
+import {z, ZodType} from "zod";
+
+export class FunctionMenuValidation {
+    static readonly CREATE: ZodType = z.object({
+        id: z.string().uuid(),
+        fm_code: z.string().trim().min(1),
+        fm_name: z.string().trim().min(1),
+        is_active: z.boolean(),
+        created_by: z.string().trim().min(1),
+        created_date: z.date()
+   });
+
+    static readonly UPDATE: ZodType = z.object({
+        fm_code: z.string().trim().min(1).optional(),
+        fm_name: z.string().trim().min(1).optional(),
+        is_active: z.boolean().optional(),
+    });
+
+    static readonly ID: ZodType = z.string().uuid();
+}
