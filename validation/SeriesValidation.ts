157da4922b88d9a045f0f595b3b11c285481a521
Ilham Hakim <ilhmhakim@gmail.com>
2025-02-07 09:12:33 +0700

    update: only 7 APIs that created on Series module, post, get all, delete, patch, get detail, get avail questions on existed series, and delete question from existed series

--- validation/SeriesValidation.ts
+++ validation/SeriesValidation.ts
@@ -4,44 +4,20 @@ export class SeriesValidation {
     static readonly CREATE: ZodType = z.object({
         series_name: z.string().min(1).max(128).trim(),
         series_code: z.string().min(1).max(16).trim().toUpperCase(),
-        is_active: z.boolean(),
-        category_id: z.number().positive(),
-        // detail: z.array(z.object({
-        //     question_id: z.string().uuid(),
-        // })),
+        questions: z.array(z.object({
+            question_id: z.string().uuid(),
+        })),
     });
 
     static readonly UPDATE: ZodType = z.object({
         series_name: z.string().min(1).optional(),
+        series_code: z.string().min(1).max(16).trim().toUpperCase().optional(),
         is_active: z.boolean().optional(),
-        updated_date: z.date()
+        questions: z.array(z.object({
+            question_id: z.string().uuid(),
+        }).optional()),
+
     });
 
     static readonly ID: ZodType = z.string().uuid();
-
-    static readonly ADDQUESTION: ZodType = z.object({
-        detail: z.array(z.object({
-            question_id: z.string().uuid(),
-        }))
-    });
-
-    static readonly QUERY: ZodType = z.object({
-        search: z.string().min(0).optional().default(""),
-        active: z.enum(["true", "false"]).optional().default("true"),
-        page: z.preprocess((value) => {
-            if (typeof value === "string") {
-                const numberValue = parseInt(value, 10);
-                return isNaN(numberValue) ? undefined : numberValue; // Jika bukan angka, biarkan sebagai undefined
-            }
-            return value; // Jika bukan string, kembalikan nilai asli
-        }, z.number().positive().optional().default(1)), // Validasi sebagai number positif, default ke 1
-        date: z.enum(['DESC', 'ASC']).optional().default('DESC'),
-        category: z.preprocess((value) => {
-            if (typeof value === "string") {
-                const numberValue = parseInt(value, 10);
-                return isNaN(numberValue) ? undefined : numberValue; // Jika bukan angka, biarkan sebagai undefined
-            }
-            return value; // Jika bukan string, kembalikan nilai asli
-        }, z.number().positive().optional())
-    })
 }
