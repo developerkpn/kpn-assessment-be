0962200c27e895b17c5bdffc0a90e8ba7c572fab
Adi Satria <adisatria@apps.ipb.ac.id>
2025-02-10 08:53:16 +0700

    refactor: update SeriesTypes to include detail field in SeriesRequests

--- types/SeriesTypes.ts
+++ types/SeriesTypes.ts
@@ -1,35 +1,29 @@
 export type SeriesRequests = {
-    series_name: string;
-    series_code: string;
-    is_active?: boolean;
-    category_id: string;
-    questions: {
-        question_id: string;
-    }[];
-}
+  series_name: string;
+  series_code: string;
+  is_active: boolean;
+  category_id: string;
+  detail: SeriesDetailRequest[]; // Pastikan ini sesuai dengan data yang diterima
+};
 
 export type SeriesHeaderRequest = {
-    id?: string;
-    series_name?: string;
-    series_code?: string;
-    is_active?: boolean;
-    category_id?: string;
-    created_by?: string;
-    created_date?: Date;
-    updated_by?: string;
-    updated_date?: Date;
-}
+  id: string;
+  series_name: string;
+  series_code: string;
+  is_active: boolean;
+  category_id: string;
+  created_by: string;
+  created_date: Date;
+};
 
 export type SeriesQuery = {
-    search?: string;
-    active?: boolean;
-    page?: number;
-    date?: string;
-    category?: number;
-}
+  search?: string;
+  active?: boolean;
+  page?: number;
+  date?: string;
+  category?: number;
+};
 
 export type SeriesDetailRequest = {
-    question_id: string;
-}
-
-
+  question_id: string;
+};
