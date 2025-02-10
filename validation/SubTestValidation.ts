90324a6fe073e22bd8b26fc354640235cce492ce
Ilham Hakim <ilhmhakim@gmail.com>
2025-02-10 13:31:37 +0700

    update: subtest fix API (post, get head, get det, delete st, patch st, get avail series)

--- validation/SubTestValidation.ts
+++ validation/SubTestValidation.ts
@@ -8,8 +8,6 @@ export class SubTestValidation {
             /^(?:[0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/,
             "Invalid time format. Must be hh:mm:ss"
         ),
-        is_active: z.boolean(),
-        category_id: z.number().positive(),
         criteria_id: z.string().uuid(),
         series: z.array(z.object({
             series_id: z.string().uuid(),
@@ -19,19 +17,16 @@ export class SubTestValidation {
     static readonly ID: ZodType = z.string().uuid();
 
     static readonly UPDATE: ZodType = z.object({
-       subtest_name: z.string().min(1).max(128).optional(),
+        subtest_name: z.string().min(1).max(128).optional(),
+        subtest_code: z.string().min(1).max(16).trim().toUpperCase().optional(),
         subtest_duration: z.string().regex(
             /^(?:[0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/,
             "Invalid time format. Must be hh:mm:ss"
         ).optional(),
         is_active: z.boolean().optional(),
-        category_id: z.number().positive().optional(),
         criteria_id: z.string().uuid().optional(),
-    });
-
-    static readonly ADDSERIES: ZodType = z.object({
         series: z.array((z.object({
             series_id: z.string().uuid(),
-        })))
+        })).optional())
     });
 }
