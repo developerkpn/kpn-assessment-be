ed2f7cdc43de6d2f885c0bf9090c04e840cfd0b0
Ilham Hakim <ilhmhakim@gmail.com>
2025-02-14 09:54:45 +0700

    update: add middleware and connection with or wo ssl

--- index.ts
+++ index.ts
@@ -8,11 +8,10 @@ import path from "path";
 import cors, { CorsOptions } from "cors";
 import cookieParser from "cookie-parser";
 import fs from "fs";
-import whitelist from "#dep/config/allowedOrigins";
-import credentials from "./middleware/credential";
+import { origins as whitelist } from "#dep/config/allowedOrigins";
+import { credentials } from "./middleware/credential";
 import router from "./routes";
 import {errorMiddleware} from "#dep/middleware/errorMiddleware";
-
 const app = express();
 const servOption = {
   cert: fs.readFileSync("./ssl/cert.pem"),
@@ -35,14 +34,12 @@ const corsOption: CorsOptions = {
 app.use("/api/static", express.static("uploads"));
 app.use((req, res, next) => {
   if (req.path.startsWith("/api/static")) {
-    return res
-        .status(404)
-        .sendFile(path.join(__dirname, "uploads", "404.html"));
+    return res.status(404).sendFile(path.join(__dirname, "uploads", "404.html"));
   }
   next();
 });
-app.use(cors(corsOption));
 
+app.use(cors(corsOption));
 app.use(credentials);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,14 +47,18 @@ app.use(cookieParser());
 app.use(router);
 app.use(errorMiddleware);
 app.use(express.static(path.join(__dirname, "public/build")));
-app.get(/^\/(?!api).*/, (req, res) => {
+app.get("/*$", (req, res) => {
   res.sendFile(path.join(__dirname, "public/build", "index.html"));
 });
-
-// const server = https.createServer(servOption, app).listen(process.env.PORT, () => {
+//
+// app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
 //   console.log(`App running on ${process.env.PORT}`);
 // });
 
-app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
-  console.log(`App running on http://localhost:${process.env.PORT}`);
+const server = https.createServer(servOption, app).listen(process.env.PORT, () => {
+  console.log(`App running on ${process.env.PORT}`);
 });
+
+// app.listen(process.env.PORT as unknown as number, "0.0.0.0", () => {
+//   console.log(`App running on http://localhost:${process.env.PORT}`);
+// });
