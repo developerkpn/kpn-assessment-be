233fb12603c9941a493d3d52b32e765178c5f19b
Ilham Hakim <ilhmhakim@gmail.com>
2025-02-18 15:05:13 +0700

    feat: Batch post, get head, get detail, patch head, add assessee manually, add assessee by file, delete assesse, delte, get batch assessee

--- controllers/BatchController.ts
+++ controllers/BatchController.ts
@@ -1,31 +1,191 @@
-// import {NextFunction, Request, Response} from "express";
-// import { v4 as uuidv4 } from "uuid";
-// import {Validation} from "#dep/validation/Validation";
-// import {BatchValidation} from "#dep/validation/BatchValidation";
-// import {createBatch} from "#dep/models/BatchModel";
-//
-// export const handleCreateBatch = async(req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         const validatedRequest = Validation.validate(BatchValidation.CREATE, req.body);
-//
-//         const batch: BatchHeaderRequest = {
-//             id:  uuidv4(),
-//             created_by: req.userDecode!.user_id,
-//             created_at: new Date(),
-//             ...validatedRequest
-//         }
-//
-//         const result = await createBatch(validatedRequest);
-//
-//         res.status(201).send({
-//
-//         });
-//
-//
-//
-//
-//
-//     } catch (e) {
-//         next(e);
-//     }
-// }
+import {NextFunction, Request, Response} from "express";
+import { v7 as uuid } from "uuid";
+import {Validation} from "#dep/validation/Validation";
+import {BatchValidation} from "#dep/validation/BatchValidation";
+import * as XLSX from 'xlsx';
+import {
+    addAssessee,
+    createBatch,
+    deleteBatch, deleteBatchAssessee,
+    getBatch, getBatchAssesses,
+    getBatchDetail,
+    updateBatch
+} from "#dep/models/BatchModel";
+import {AdminWebValidation} from "#dep/validation/AdminWebValidation";
+import {BatchAssessee, BatchHeader, BatchHeadUpdate} from "#dep/types/BatchTypes";
+
+export const handleCreateBatch = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedRequest = Validation.validate(BatchValidation.CREATE, req.body);
+
+        const batch: BatchHeader = {
+            id:  uuid(),
+            created_by: req.userDecode!.user_id,
+            created_at: new Date(),
+            ...validatedRequest
+        }
+
+        const result = await createBatch(batch);
+
+        res.status(201).send({
+            message: `Batch with code ${result} is created successfully!`,
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleGetBatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await getBatch();
+
+        res.status(200).send({
+            message: "Success!",
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleUpdateBatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+        const validatedRequest = Validation.validate(BatchValidation.UPDATE, req.body);
+        const batchUpdate: BatchHeadUpdate = {
+            id: validatedId,
+            updated_by: req.userDecode?.user_id,
+            updated_at: new Date(),
+            ...validatedRequest
+        }
+        const result = await updateBatch(validatedId, batchUpdate);
+
+        res.status(201).send({
+            message: `Batch with code ${result} is updated successfully!`,
+        })
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleDeleteBatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+
+        const result = await deleteBatch(validatedId);
+
+        res.status(200).send({
+            message: `Batch with code ${result} is deleted successfully`,
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleAddAssesseeManually = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+        const validatedRequest = Validation.validate(BatchValidation.ADDASSESSEEMANUALLY, req.body);
+
+        const assessee: BatchAssessee = {
+            id: uuid(),
+            batch_id: validatedId,
+            ...validatedRequest
+        }
+
+        await addAssessee(assessee);
+
+        res.status(201).send({
+            message: "Assessee is successfully added!",
+        })
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleGetBatchDetail = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+
+        const result = await getBatchDetail(validatedId);
+
+        res.status(200).send({
+            message: `Success!`,
+            data: result
+        })
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleGetBatchAssessees = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+
+        const result = await getBatchAssesses(validatedId)
+
+        res.status(200).send({
+            message: "Success!",
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleDeleteBatchAssessee = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const validatedBatchId = Validation.validate(BatchValidation.ID, req.params.id);
+        const validateAssesseeId = Validation.validate(BatchValidation.ID, req.params.assesseeId);
+        console.log("halo")
+        await deleteBatchAssessee(validatedBatchId, validateAssesseeId);
+        console.log("halo 2")
+        res.status(200).send({
+            message: "Success!"
+        })
+    } catch (e) {
+        next(e);
+    }
+}
+
+export const handleAddAssesseeByFile = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.file) {
+        res.status(400).send('File tidak ditemukan.');
+        return;
+    }
+
+    try {
+        const validatedId = Validation.validate(BatchValidation.ID, req.params.id);
+        const workbook = XLSX.read(req.file.buffer, { type: 'buffer' });
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+            defval: null,
+            raw: false
+        });
+
+        const filteredData = jsonData.map((row: any) => {
+            const result = {
+                id: uuid(),
+                batch_id: validatedId,
+                assessee_nik: row.assessee_nik,
+                assessee_name: row.assessee_name,
+                assessee_email: row.assessee_email,
+            };
+            return result;
+        });
+
+        console.log(filteredData);
+        const validatedAssessee = Validation.validate(BatchValidation.ASSESSEE, filteredData);
+
+        await addAssessee(validatedAssessee);
+
+        res.status(200).send({
+            message: "Success!"
+        });
+
+    } catch (e) {
+        next(e);
+    }
+}
--- validation/BatchValidation.ts
+++ validation/BatchValidation.ts
@@ -4,34 +4,44 @@ export class BatchValidation {
     static readonly CREATE: ZodType = z.object({
         batch_name: z.string().trim().min(1),
         batch_code: z.string().trim().min(1).max(16).toUpperCase(),
-        group_id: z.string().uuid(),
+        grouptest_id: z.string().uuid(),
         bu_id: z.string().uuid(),
         function_id: z.string().uuid(),
+        template_email_id: z.string().uuid(),
         start_period: z.string().min(1),
         end_period: z.string().min(1),
-        randomized_question: z.boolean(),
-        randomized_test_series: z.boolean(),
-        mic: z.boolean(),
-        screenshot: z.boolean(),
+        is_mic: z.boolean(),
+        is_screenshot: z.boolean(),
         note: z.string().trim().min(1),
-        template_email_id: z.string().uuid()
     });
 
     static readonly UPDATE: ZodType = z.object({
         batch_name: z.string().trim().min(1).optional(),
         batch_code: z.string().trim().min(1).max(16).toUpperCase().optional(),
-        group_id: z.string().uuid().optional(),
+        grouptest_id: z.string().uuid().optional(),
         bu_id: z.string().uuid().optional(),
         function_id: z.string().uuid().optional(),
+        template_email_id: z.string().uuid().optional(),
         start_period: z.string().min(1).optional(),
         end_period: z.string().min(1).optional(),
-        randomized_question: z.boolean().optional(),
-        randomized_test_series: z.boolean().optional(),
-        mic: z.boolean().optional(),
-        screenshot: z.boolean().optional(),
+        is_mic: z.boolean().optional(),
+        is_screenshot: z.boolean().optional(),
         note: z.string().trim().min(1).optional(),
-        template_email_id: z.string().uuid().optional()
     })
 
+    static readonly ADDASSESSEEMANUALLY: z.ZodSchema = z.object({
+        assessee_nik: z.string().length(16),
+        assessee_name: z.string().trim().min(1),
+        assessee_email: z.string().email()
+    });
+
     static readonly ID: ZodType = z.string().uuid();
+
+    static readonly ASSESSEE: ZodType = z.array(z.object({
+        id: z.string().uuid(),
+        batch_id: z.string().uuid(),
+        assessee_nik: z.string().trim().length(16),
+        assessee_name: z.string().trim().min(1),
+        assessee_email: z.string().email()
+    }))
 }
