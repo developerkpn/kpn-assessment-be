6592bb1c0818d60d066701d5ea94e58caf0e46ab
Ilham Hakim <ilhmhakim@gmail.com>
2025-03-13 16:03:45 +0700

    update: add description into test

--- validation/TestValidation.ts
+++ validation/TestValidation.ts
@@ -2,8 +2,9 @@ import {z, ZodType} from "zod";
 
 export class TestValidation {
     static readonly CREATE: ZodType = z.object({
-        test_name: z.string().min(1).max(128).trim(),
-        test_code: z.string().min(1).max(16).trim().toUpperCase(),
+        test_name: z.string().trim().min(1).max(128),
+        test_code: z.string().trim().min(1).max(16).toUpperCase(),
+        description: z.string().trim().min(1),
         subtests: z.array(z.object({
             subtest_id: z.string().uuid(),
         })),
@@ -12,11 +13,12 @@ export class TestValidation {
     static readonly ID: ZodType = z.string().uuid();
 
     static readonly UPDATE: ZodType = z.object({
-        test_name: z.string().min(1).max(128).optional(),
-        test_code: z.string().min(1).max(16).trim().toUpperCase().optional(),
+        test_name: z.string().trim().min(1).max(128).optional(),
+        test_code: z.string().trim().min(1).max(16).toUpperCase().optional(),
         is_active: z.boolean().optional(),
+        description: z.string().trim().min(1).optional(),
         subtests: z.array((z.object({
             subtest_id: z.string().uuid(),
-        })).optional())
+        })).optional()).optional()
     });
 }
