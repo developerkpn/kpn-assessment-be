2949fdc41140a29979406638ae92f948c82978ce
Ilham Hakim <ilhmhakim@gmail.com>
2025-03-18 09:05:24 +0700

    update: storing answer one by one and tracking taken questions

--- routes/transactions/Assessment.ts
+++ routes/transactions/Assessment.ts
@@ -5,11 +5,13 @@ import {
   handleGetAsssessmentQuestion,
   handleGetBatchDetail,
   handleStoreAnswer,
-  handleVideoProctoring,
+  handleSubmissionConfirmation,
 } from "#dep/controllers/transaction/AssessmentController";
 import { handleGetQuestion } from "#dep/controllers/QuestionController";
 
 const Assessment = Router();
+
+Assessment.put("/subtest/submission", handleSubmissionConfirmation);
 
 Assessment.get("/:token/batch", handleGetBatchDetail);
 // Assessment.get("/:token", handleStartProgress);
@@ -19,8 +21,12 @@ Assessment.get("/:token/test/subtest/:id", handleGetAsssessmentQuestion);
 Assessment.get("/:token/test/:id", handleGetAssessmentSubTest);
 // Assessment.post("/test/:testId/subtest/:subtestId/start");
 Assessment.post("/:token/subtest/submission", handleStoreAnswer);
-Assessment.post("/video", handleVideoProctoring);
+// Assessment.post("/video", handleVideoProctoring);
 // Assessment.get("/")
 // Assessment.get("/", );
 
+// Assessment.get("/batch/:token", handleGetBatchDetail);
+// Assessment.get("/test/:token", handleGetAssessmentTest);
+// Assessment.get("/test/:token/:testId", handleGetAssessmentSubTest);
+// Assessment.get("/test/subtest/:token/:testId, ", handleGetAsssessmentQuestion);
 export default Assessment;
