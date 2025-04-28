7c669f620d6f33b20e2366bb2a0d25e539ca69dd
Ilham Hakim <ilhmhakim@gmail.com>
2025-04-28 16:24:33 +0700

    update: add criteria to category

--- validation/CategoryValidation.ts
+++ validation/CategoryValidation.ts
@@ -4,6 +4,7 @@ export class CategoryValidation {
   static readonly CREATE: ZodType = z.object({
     category_name: z.string().trim().min(1).max(128),
     category_code: z.string().trim().min(1).max(16).toUpperCase(),
+    criteria_id: z.string().uuid(),
     created_by: z.string().trim().length(36),
     created_at: z.date(),
     is_active: z.boolean(),
@@ -12,6 +13,7 @@ export class CategoryValidation {
   static readonly UPDATE: ZodType = z
     .object({
       category_name: z.string().trim().min(1).max(128).optional(),
+      criteria_id: z.string().uuid(),
       updated_by: z.string().trim().min(1),
       updated_at: z.date(),
       is_active: z.boolean().optional(),
