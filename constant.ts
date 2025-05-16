99e1afd5e0e890b6e09b18f2565c8863b60f80e0
Ilham Hakim <ilhmhakim@gmail.com>
2025-05-16 10:40:17 +0700

    update: add report guide id

--- constant.ts
+++ constant.ts
@@ -4,3 +4,4 @@ export const refreshExpiry = "6h";
 export const TERMS_ID = "d1d3d3bd-f06c-4dca-90fb-7ef043c6508b";
 export const PP_ID = "2b2818b2-e5cd-4d67-a024-9a597b5e3132";
 export const BRIEF_ID = "ad21912c-b084-493c-9079-0995388f5458";
+export const REPORT_GUIDE_ID = "0196cd97-d404-7efe-8bd3-122f524fa685";
