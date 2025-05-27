e23ec4446062487c94234f4430a56a426fd782b1
Ilham Hakim <ilhmhakim@gmail.com>
2025-05-27 16:21:01 +0700

    update: guide content can be added from design or manually from master

--- routes/report/Report.ts
+++ routes/report/Report.ts
@@ -7,6 +7,7 @@ import {
   handleGetReportGuide,
   handleReportPersonal,
   handleReportPreview,
+  handleStoreReportGuide,
   handleUpdateReportDesign,
   handleUpdateReportGuide,
   handleUploadReportPDF,
@@ -15,11 +16,12 @@ import {
 const Report = Router();
 
 Report.get("/preview", handleReportPreview);
-Report.put("/guide", handleUpdateReportGuide);
+Report.post("/guide", handleStoreReportGuide);
 Report.get("/guide", handleGetReportGuide);
 Report.post("/pdf", handleUploadReportPDF);
 Report.post("/result", handleReportPersonal);
 Report.post("/design", handleCreateReportForBatch);
+Report.put("/guide/:id", handleUpdateReportGuide);
 Report.get("/design/:batchId", handleGetReportDesignDetail);
 Report.patch("/design/:batchId", handleUpdateReportDesign);
 Report.get("/template/:batchId", handleGetBatchInformationForReport);
