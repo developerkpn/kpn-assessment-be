cf5be5bbe0f3fe13b371cc73e7ec87c0a2d1e60b
tectanagrand <rtektano@gmail.com>
2025-06-17 12:24:17 +0700

    fix : assessee auth and report proctoring&uploadproctoring

--- routes/transactions/Assessee.ts
+++ routes/transactions/Assessee.ts
@@ -10,12 +10,13 @@ import {
   handleUpdateExternalAssesseeInformation,
 } from "@/controllers/transaction/AssesseeController.js";
 import { isAuthAssessee } from "@/middleware/auth.js";
+import { errorMiddleware } from "@/middleware/errorMiddleware";
 
 const Assessee = Router();
 Assessee.post("/registration", handleExternalRegistration);
 Assessee.post("/login", handleExternalLogin);
 Assessee.get("/dashboard", isAuthAssessee, handleGetAssignedBatch);
-Assessee.get("/profile", isAuthAssessee, handleGetAssesseeInformation);
+Assessee.get("/profile", isAuthAssessee, handleGetAssesseeInformation, errorMiddleware);
 Assessee.get("/resettoken", handleResetToken);
 Assessee.patch("/profile", isAuthAssessee, handleUpdateExternalAssesseeInformation);
 Assessee.delete("/logout");
