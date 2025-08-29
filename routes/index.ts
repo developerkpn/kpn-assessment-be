60b6db936be7feb12153ba7aa3aa3bbdd0596b39
tectanagrand <rtektano@gmail.com>
2025-08-29 09:33:38 +0700

    fix : add forceto, fix merging from dev

--- routes/index.ts
+++ routes/index.ts
@@ -25,6 +25,7 @@ import Assessee from "@/routes/transactions/Assessee.js";
 import Assessment from "@/routes/transactions/Assessment.js";
 import Proctoring from "./transactions/Proctoring.js";
 // import Guideline from "@/routes/GuideLine.js";
+import Translation from "./Translation.js";
 import { errorMiddleware } from "@/middleware/errorMiddleware.js";
 
 //@using router
@@ -49,6 +50,8 @@ router.use("/api/assessment", Assessment);
 router.use("/api/proctoring", Proctoring);
 router.use("/api/assessee", Assessee);
 router.use("/api/report", isAuth, Report);
+router.use("/api/languages", isAuth, Language);
+router.use("/api/translation", isAuth, Translation);
 // router.use("/api/guideline", Guideline, errorMiddleware);
 router.use("/api/check", (req, res) => {
   res.status(200).send({
