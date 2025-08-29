2136e8d6ba1f62b9e47cd9c2f5c120fde9b299ef
tectanagrand <rtektano@gmail.com>
2025-08-29 09:33:38 +0700

    fix : add forceto, fix merging from dev

--- routes/index.ts
+++ routes/index.ts
@@ -54,6 +54,8 @@ router.use("/api/translation", isAuth, Translation);
 router.use("/api/report", Report);
 router.use("/api/languages", isAuth, Language);
 router.use("/api/report", isAuth, Report);
+router.use("/api/languages", isAuth, Language);
+router.use("/api/translation", isAuth, Translation);
 // router.use("/api/guideline", Guideline, errorMiddleware);
 router.use("/api/check", (req, res) => {
   res.status(200).send({
