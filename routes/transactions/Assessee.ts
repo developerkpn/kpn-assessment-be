8d12126d859235eb1fb0b3ed1d550a721b6048cb
BenZeta <benpardede.hacktiv8@gmail.com>
2025-09-23 11:02:38 +0700

    feat: imagemiddleware for handling uploading image, assessee profile photo logic

--- routes/transactions/Assessee.ts
+++ routes/transactions/Assessee.ts
@@ -8,9 +8,12 @@ import {
   handleGetAssesseeInformation,
   handleResetToken,
   handleUpdateExternalAssesseeInformation,
+  handleUploadProfilePhoto,
+  handleGetProfilePhoto,
 } from "@/controllers/transaction/AssesseeController.js";
 import { isAuthAssessee } from "@/middleware/auth.js";
 import { errorMiddleware } from "@/middleware/errorMiddleware.js";
+import { uploadSingleImage } from "@/middleware/imageMiddleware.js";
 
 const Assessee = Router();
 Assessee.post("/registration", handleExternalRegistration);
@@ -19,6 +22,8 @@ Assessee.get("/dashboard", isAuthAssessee, handleGetAssignedBatch);
 Assessee.get("/profile", isAuthAssessee, handleGetAssesseeInformation, errorMiddleware);
 Assessee.get("/resettoken", handleResetToken);
 Assessee.patch("/profile", isAuthAssessee, handleUpdateExternalAssesseeInformation);
+Assessee.post("/upload-profile-photo", isAuthAssessee, uploadSingleImage, handleUploadProfilePhoto);
+Assessee.get("/profile-photo/:userId", handleGetProfilePhoto);
 Assessee.delete("/logout");
 Assessee.get("/:token", handleAssesseeEntry);
 Assessee.get("/isreg/:email", handleCheckExternUser);
